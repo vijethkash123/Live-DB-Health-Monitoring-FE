9c1ead41cd6366f81cd1e5a6c37d7749cdd2002f
Vijeth Kashyap <vijeth.kashyap@BRILLIO.COM>
2019-12-10 16:40:55 +0530

    More info changed Vijeth Brillio PC 2 10-12

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,63 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
-    selector: 'dashboard-cmp',
-    moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+  selector: 'dashboard-cmp',
+  moduleId: module.id,
+  templateUrl: 'dashboard.component.html'
 
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
 
   project_data: any[];
   codex_data: any;
+  isCodex: boolean;
+  isCustomer: boolean;
+  isDi: boolean;
+  constructor(private http: HttpClient) {
 
-  constructor(private http:HttpClient){
- 
   }
 
 
   ngOnInit() {
- 
-    this.project({"cmd_id":"project_data"});
-    this.codex({"cmd_id":"codex_data"})
-    
-    }
-   
 
-   
-    
-   project(rData){
-    //this.showSpinner = true;
-    this.getData(rData).subscribe((data:any) => {
-    //this.showSpinner = false;
-    // this.resp.push(data);
-    console.log("Success",data);
-    this.project_data = data;
-    },
-    error => { 
-    // this.showSpinner = false;
-    });
-    
-   }
+    this.project({ "cmd_id": "project_data" });
+    this.codex({ "cmd_id": "codex_data" })
 
-   codex(rData){
+  }
+
+
+
+
+  project(rData) {
     //this.showSpinner = true;
-    this.getData(rData).subscribe((data:any) => {
-    //this.showSpinner = false;
-    // this.resp.push(data);
-    console.log("Success",data);
-    this.codex_data = data;
+    this.getData(rData).subscribe((data: any) => {
+      //this.showSpinner = false;
+      // this.resp.push(data);
+      console.log("Success", data);
+      this.project_data = data;
     },
-    error => { 
-    // this.showSpinner = false;
-    });
-    
-   }
-   getData(rData){
-    return this.http.post('http://13.126.189.69:80/dev-api/BP',rData);
-   }
+      error => {
+        // this.showSpinner = false;
+      });
+
+  }
+
+  codex(rData) {
+    //this.showSpinner = true;
+    this.getData(rData).subscribe((data: any) => {
+      //this.showSpinner = false;
+      // this.resp.push(data);
+      console.log("Success", data);
+      this.codex_data = data;
+    },
+      error => {
+        // this.showSpinner = false;
+      });
+
+  }
+  getData(rData) {
+    return this.http.post('http://13.126.189.69:80/dev-api/BP', rData);
+  }
+
+  codex_call() {
+    this.isCodex = true;
+    this.isCustomer = false;
+    this.isDi = false;
+  }
+  customer_call() {
+    this.isCodex = false;
+    this.isCustomer = true;
+    this.isDi = false;
+  }
+  di_call() {
+    this.isDi = true;
+    this.isCodex = false;
+    this.isCustomer = false;
+  }
 }

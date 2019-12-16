4d5a06f126a621a6ebe1975ca26f3ac4305850f2
Vijeth Kashyap <vijeth.kashyap@BRILLIO.COM>
2019-12-16 15:15:15 +0530

    Welcome fixed Vijeth Brillio PC 1 16-12

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -17,6 +17,7 @@ export class DashboardComponent implements OnInit {
   isCodex: boolean;
   isCustomer: boolean;
   isDi: boolean;
+  isWelcome: boolean;
   constructor(private http: HttpClient) {
 
   }
@@ -26,7 +27,7 @@ export class DashboardComponent implements OnInit {
 
     this.project({ "cmd_id": "project_data" });
     this.codex({ "cmd_id": "codex_data" })
-
+    this.isWelcome=true;
   }
 
 
@@ -67,15 +68,18 @@ export class DashboardComponent implements OnInit {
     this.isCodex = true;
     this.isCustomer = false;
     this.isDi = false;
+    this.isWelcome=false;
   }
   customer_call() {
     this.isCodex = false;
     this.isCustomer = true;
     this.isDi = false;
+    this.isWelcome=false;
   }
   di_call() {
     this.isDi = true;
     this.isCodex = false;
     this.isCustomer = false;
+    this.isWelcome=false;
   }
 }

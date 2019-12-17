9306d1b8d397e5f88680f65dd247ed54a763c5b8
Umesh Kumar S <umesh.s@BRILLIO.COM>
2019-12-17 17:37:41 +0530

    Umesh

--- src/app/pages/proposal/proposal.component.ts
+++ src/app/pages/proposal/proposal.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 })
 
 export class ProposalComponent{
+    show:boolean
+    p1:boolean;
+    p2:boolean;
+    p3:boolean;
+    p4:boolean;
+    toggle1(){
+        this.p1 = true;
+        this.show = false;
+    }
+    toggle2(){
+        this.p2 = true;
+        this.show = false;
+    }
+    toggle3(){
+        this.p3 = true;
+        this.show = false;
+    }
+    toggle4(){
+        this.p4 = true;
+        this.show = false;
+    }
+    main(){
+        this.show = true;
+        this.p1 =false
+        this.p2 =false
+        this.p3 =false
+        this.p4 =false
+    }
 }
